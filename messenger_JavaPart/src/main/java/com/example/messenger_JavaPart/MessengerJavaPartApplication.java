7187628f6d732a87925fa3618224023693374836
danil-kkrk <hirofool502@gmail.com>
2025-02-24 20:14:41 +0300

    start

---
+++ messenger_JavaPart/src/main/java/com/example/messenger_JavaPart/MessengerJavaPartApplication.java
@@ -0,0 +1,13 @@
+package com.example.messenger_JavaPart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerJavaPartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessengerJavaPartApplication.class, args);
+	}
+
+}
---
+++ messenger_JavaPart/src/test/java/com/example/messenger_JavaPart/MessengerJavaPartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.messenger_JavaPart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerJavaPartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
